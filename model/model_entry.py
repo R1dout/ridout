222986f2e44e3b7d77cea38fa45af27776aac99f
R1dout <129485080+R1dout@users.noreply.github.com>
2023-04-07 14:13:00 +0800

    Update model_entry.py

--- model/model_entry.py
+++ model/model_entry.py
@@ -1,16 +1,9 @@
-from model.base.fcn import CustomFcn
-from model.best.fcn import DeepLabv3Fcn
-from model.better.fcn import Resnet101Fcn
-from model.sota.fcn import LightFcn
 import torch.nn as nn
 
 
 def select_model(args):
     type2model = {
-        'resnet50_fcn': CustomFcn(args),
-        'resnet101_fcn': Resnet101Fcn(args),
-        'deeplabv3_fcn': DeepLabv3Fcn(args),
-        'mobilnetv3_fcn': LightFcn(args)
+
     }
     model = type2model[args.model_type]
     return model
