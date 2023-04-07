c22ec157448dbd7bff4ce61965b1016705fda4f4
R1dout <129485080+R1dout@users.noreply.github.com>
2023-04-07 10:31:12 +0800

    Update model_entry.py

--- model/model_entry.py
+++ model/model_entry.py
@@ -14,8 +14,3 @@ def select_model(args):
     }
     model = type2model[args.model_type]
     return model
-
-
-def equip_multi_gpu(model, args):
-    model = nn.DataParallel(model, device_ids=args.gpus)
-    return model
