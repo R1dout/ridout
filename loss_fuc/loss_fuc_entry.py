8a001b10771a5fc045de68934602fee0f09f2b2d
R1dout <129485080+R1dout@users.noreply.github.com>
2023-04-10 15:29:14 +0800

    Update loss_fuc_entry.py

--- loss_fuc/loss_fuc_entry.py
+++ loss_fuc/loss_fuc_entry.py
@@ -2,8 +2,8 @@ import torch.nn as nn
 
 def select_loss_fuc(args):
   type2loss = {
-    'l1':nn.L1Loss()
-    'ce':nn.CrossEntropyLoss()
+    'l1':nn.L1Loss(),
+    'ce':nn.CrossEntropyLoss(),
     'mse':nn.MSELoss()
   }
   loss_fuc = type2loss[args.model_type]
